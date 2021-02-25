ceacc07b31c327a503d639a9a2872a2eb7afba93
Paula Waite <pawaite@madisoncollege.edu>
2021-02-25 15:21:19 -0600

    adds the bit to call the born route and write data to the console

--- src/GitHubBornOn.js
+++ src/GitHubBornOn.js
@@ -1,8 +1,39 @@
-// React
-import React from 'react';
-
+// Import useState and useEffect hooks from React
+import 
+  React
+  , { 
+    useState
+    , useEffect } 
+    
+  from 'react';
+// Import the API category from AWS Amplify
+import { API } from 'aws-amplify'  // swirly because not default export
 
 export const GitHubBornOn = () => { 
+ 
+// Define a constant that is a function to call API
+  const getGitHubInfoByUser = async () => { // an asynch lambda that goes to
+
+    try {
+      
+      const data = await API.get('cryptoapi', `/born`)
+      console.log(data)
+    }
+
+    catch (err) {
+      console.error(err);
+    }
+    
+  };
+
+  useEffect(
+    () => {
+      getGitHubInfoByUser();
+    }
+    , []
+  );
+
+
     // start with placeholder txt first!
     return(
         <h2>
