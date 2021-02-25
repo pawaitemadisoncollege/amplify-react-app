08c1903d51d744bb453ef5a92859f637f0ed0556
Paula Waite <pawaite@madisoncollege.edu>
2021-02-24 18:10:30 -0600

    Added loading to demonstrate useState

--- src/App.js
+++ src/App.js
@@ -19,9 +19,14 @@ const App = () => {
 
   // Define a constant that is a function to all API
   const fetchCoins = async () => { // an asynch lambda that goes to
+
+    updateLoading(true);
+
     const { limit, start } = input
     const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`)
     updateCoins(data.coins)  // updateCoins was given to us from useState (react stuff!)
+    updateLoading(false);
+
   };
 
   // Call fetchCoins function when component loads
@@ -43,6 +48,8 @@ const updateInputValues = (type, value) => {
   });
 };
 
+const [loading, updateLoading] = useState(true);
+
   return (
     <div className="App">
 
@@ -55,12 +62,13 @@ const updateInputValues = (type, value) => {
         onChange={e => updateInputValues('limit', e.target.value)}
         placeholder="Enter a limit"
       />
-      
-
 
       <button onClick={fetchCoins}>Fetch Coins</button>
       
+    {loading && <h2>Loading....</h2>}
+
       {
+        !loading && 
         coins.map(  // for each item... 
           (coin, index) => (
           <div 
